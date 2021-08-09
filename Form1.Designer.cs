2242257100591049410a6f2b8e4deb993b7cdb7a
Gluttony-Meow <glutony0910@gmail.com>
2021-08-09 16:49:22 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Ontap01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbClass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpFinalDate = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbRank = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Class = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FinalGrade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FinalDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rank = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Achievements = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(221, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản Lý Sinh Viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(65, 57);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(135, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // tbClass
+            // 
+            this.tbClass.Location = new System.Drawing.Point(65, 143);
+            this.tbClass.Name = "tbClass";
+            this.tbClass.Size = new System.Drawing.Size(135, 20);
+            this.tbClass.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Location = new System.Drawing.Point(65, 100);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(47, 17);
+            this.rbtnMale.TabIndex = 7;
+            this.rbtnMale.TabStop = true;
+            this.rbtnMale.Text = "Nam";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Location = new System.Drawing.Point(136, 100);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(39, 17);
+            this.rbtnFemale.TabIndex = 8;
+            this.rbtnFemale.TabStop = true;
+            this.rbtnFemale.Text = "Nữ";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(65, 189);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(135, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            // 
+            // dtpFinalDate
+            // 
+            this.dtpFinalDate.Location = new System.Drawing.Point(65, 229);
+            this.dtpFinalDate.Name = "dtpFinalDate";
+            this.dtpFinalDate.Size = new System.Drawing.Size(203, 20);
+            this.dtpFinalDate.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 272);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
+            // 
+            // cbRank
+            // 
+            this.cbRank.FormattingEnabled = true;
+            this.cbRank.Location = new System.Drawing.Point(65, 269);
+            this.cbRank.Name = "cbRank";
+            this.cbRank.Size = new System.Drawing.Size(121, 21);
+            this.cbRank.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 320);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "label8";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Sex,
+            this.Class,
+            this.FinalGrade,
+            this.FinalDate,
+            this.Rank,
+            this.Achievements});
+            this.dataGridView1.Location = new System.Drawing.Point(284, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(674, 403);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            ""});
+            this.listBox1.Location = new System.Drawing.Point(66, 312);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(173, 95);
+            this.listBox1.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 32);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Tên SV";
+            this.Name.Name = "Name";
+            // 
+            // Sex
+            // 
+            this.Sex.HeaderText = "Giới tính";
+            this.Sex.Name = "Sex";
+            // 
+            // Class
+            // 
+            this.Class.HeaderText = "Lớp Học";
+            this.Class.Name = "Class";
+            // 
+            // FinalGrade
+            // 
+            this.FinalGrade.HeaderText = "Điểm Tổng Kết";
+            this.FinalGrade.Name = "FinalGrade";
+            // 
+            // FinalDate
+            // 
+            this.FinalDate.HeaderText = "Ngày Tổng Kết";
+            this.FinalDate.Name = "FinalDate";
+            // 
+            // Rank
+            // 
+            this.Rank.HeaderText = "Xếp Hạng";
+            this.Rank.Name = "Rank";
+            // 
+            // Achievements
+            // 
+            this.Achievements.HeaderText = "Thành tích";
+            this.Achievements.Name = "Achievements";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 526);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbRank);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dtpFinalDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.rbtnFemale);
+            this.Controls.Add(this.rbtnMale);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbClass);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            ///Name = "Form1";
+            this.Text = "QLSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbClass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpFinalDate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbRank;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Class;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FinalGrade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FinalDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rank;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Achievements;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ontap01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+          
+            
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-GBBCS2R;Initial Catalog=SinhVien;Integrated Security=True");
+        private void ketnoicsdl()
+        {
+            con.Open();
+            string sql = "select * from SV";  // lay het du lieu trong bang sinh vien
+            SqlCommand com = new SqlCommand(sql, con); //bat dau truy van
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
+            DataTable dt = new DataTable(); //tạo một kho ảo để lưu trữ dữ liệu
+            da.Fill(dt);  // đổ dữ liệu vào kho
+            con.Close();  // đóng kết nối
+            dataGridView1.DataSource = dt; //đổ dữ liệu vào datagridview
+        }
+        void add() {//nhận dữ liệu từ btn 01
+
+            int gender = 0;
+            if (rbtnMale.Checked == true)
+                gender = 0;
+            else if (rbtnFemale.Checked == true)
+                gender = 1;
+            string sqlInsert = "INSERT INTO SV(Name ,Sex, Class, FinalGrade, FinalDate, Rank, Achievements) values (@Name,@Sex,@Class,@FinalGrade, @FinalDate,@Rank, @Achievements) ";
+            SqlCommand com = new SqlCommand(sqlInsert, con);
+            com.Parameters.AddWithValue("@Name", tbName.Text);// dữ liệu 01
+            com.Parameters.AddWithValue("@Sex", gender);
+            com.Parameters.AddWithValue("@Class", tbClass.Text);
+            com.Parameters.AddWithValue("@FinalGrade", numericUpDown1.Text);
+            com.Parameters.AddWithValue("@FinalDate", Convert.ToDateTime(dtpFinalDate));
+            com.Parameters.AddWithValue("@Rank", cbRank.Text);
+            com.Parameters.AddWithValue("@Achievements",listBox1.Text);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //validation ở đây 
+            if (tbName.Text == string.Empty)
+                MessageBox.Show("TextBox is Empty");
+            if (tbClass.Text == string.Empty)
+                MessageBox.Show("TextBox is Empty");
+            if (rbtnMale.Checked == false && rbtnFemale.Checked == false)
+                MessageBox.Show("Choose Male or Female");
+            if (numericUpDown1.Value < 0)
+                MessageBox.Show("FinalGrade must be >=0");
+            if (dtpFinalDate.Value >= DateTime.Now)
+                MessageBox.Show("FinalDate must be greater than now");
+            add();//  truyền dữ liệu đã đươc validate vào trong hàm add
+            ketnoicsdl();
+        }
+    }
+}
